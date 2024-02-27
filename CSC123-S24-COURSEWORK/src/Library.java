6392ac9844e9b46c51f11e629a50a2879b6d032b
mflores439 <mflores439@toromail.csudh.edu>
2024-02-26 22:03:16 -0800

    Added toString() and equals() along with test for it

--- CSC123-S24-COURSEWORK/src/Library.java
+++ CSC123-S24-COURSEWORK/src/Library.java
@@ -1,4 +1,20 @@
 
 public class Library {
-
+	
+	String name;
+	String address;
+	
+	public Library(String name, String address) {
+		
+		this.name=name;
+		this.address=address;
+	}
+	
+	public String toString() {
+		return  name + " , in " + address;
+	}
+	
+	public boolean equals(Library l) {
+		return name.equals(l.name)&& address.equals(l.address); 
+	}
 }
--- CSC123-S24-COURSEWORK/src/LibraryTestBook.java
+++ CSC123-S24-COURSEWORK/src/LibraryTestBook.java
@@ -2,8 +2,17 @@
 public class LibraryTestBook {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Library library = new Library("Main Library", "Los Angeles California");
+		
+		Book book1 = new Book("Harry Potter and the Order of the Phoenix", "J.K. Rowling",766, "0439682584123");
+		Book book2 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, "9781782808084");
+		Book book3 = new Book("Moby Dick", "Herman Melville", 700, "9781234567897");
+		
+		System.out.println("Library: " + library);
+		System.out.println("Book 1; " + book1);
+		System.out.println("Book 2; " + book2);
+		System.out.println("Book 3; " + book3);
 	}
 
 }
